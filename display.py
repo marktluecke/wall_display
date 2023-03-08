eb732bcb8146928643556fa6fc8cefbbcfee828b
marktluecke <henrijamespotter+gh@gmail.com>
2023-03-08 16:36:30 +0000

    erste funktionierende Version

--- display.py
+++ display.py
@@ -2,37 +2,117 @@ from gpiozero import LED
 from time import sleep
 
 class Display:
-        def __init__(self, pin_point, pin_up, pin_up_left, pin_up_right, pin_middle, pin_down_left, pin_down_right, pin_down):
-                self.pin_point = LED(pin_point)
-                self.pin_up = LED(pin_up)
-                self.pin_up_left = LED(pin_up_left)
-                self.pin_up_right = LED(pin_up_right)
-                self.pin_middle = LED(pin_middle)
-                self.pin_down_left = LED(pin_down_left)
-                self.pin_down_right = LED(pin_down_right)
-                self.pin_down = LED(pin_down)
+    def __init__(self, pin_point, pin_up, pin_up_left, pin_up_right, pin_middle, pin_down_left, pin_down_right, pin_down):
+        self.led_point = LED(pin_point)
+        self.led_up = LED(pin_up)
+        self.led_up_left = LED(pin_up_left)
+        self.led_up_right = LED(pin_up_right)
+        self.led_middle = LED(pin_middle)
+        self.led_down_left = LED(pin_down_left)
+        self.led_down_right = LED(pin_down_right)
+        self.led_down = LED(pin_down)
 
-	
-	def ledTest():
-		self.pin_point.on()
-                self.pin_up.on()
-                self.pin_up_left.on()
-                self.pin_up_right.on()
-                self.pin_middle.on()
-                self.pin_down_left.on()
-                self.pin_down_right.on()
-                self.pin_down.on()
-		
-		sleep(5)
-		
-		self.pin_point.off()
-                self.pin_up.off()
-                self.pin_up_left.off()
-                self.pin_up_right.off()
-                self.pin_middle.off()
-                self.pin_down_left.off()
-                self.pin_down_right.off()
-                self.pin_down.off()
-		
-		
-Display(2,3,4,17,27,22,10,9)
+        self.zero = [[1],[1,1],[0],[1,1],[1],[0]]
+
+        self.one = [[0],[0,1],[0],[0,1],[0],[0]]
+
+        self.two = [[1],[0,1],[1],[1,0],[1],[0]]
+
+        self.three = [[1],[0,1],[1],[0,1],[1],[0]]
+
+        self.four = [[0],[1,1],[1],[0,1],[0],[0]]
+
+        self.five = [[1],[1,0],[1],[0,1],[1],[0]]
+
+        self.six = [[1],[1,0],[1],[1,1],[1],[0]]
+
+        self.seven = [[1],
+                    [0,1],
+                    [0],
+                    [0,1],
+                    [0],
+                    [0]]
+
+        self.eight = [[1],[1,1],[1],[1,1],[1],[0]]
+
+        self.nine = [[1],[1,1],[1],[0,1],[1],[0]]
+
+        self.digits = [self.zero, self.one, self.two, self.three, self.four, self.five, self.six, self.seven, self.eight, self.nine]
+
+    def ledTest(self):
+        self.led_point.on()
+        self.led_up.on()
+        self.led_up_left.on()
+        self.led_up_right.on()
+        self.led_middle.on()
+        self.led_down_left.on()
+        self.led_down_right.on()
+        self.led_down.on()
+
+        sleep(1000)
+
+        self.led_point.off()
+        self.led_up.off()
+        self.led_up_left.off()
+        self.led_up_right.off()
+        self.led_middle.off()
+        self.led_down_left.off()
+        self.led_down_right.off()
+        self.led_down.off()
+
+
+    def reset(self):
+        self.led_point.off()
+        self.led_up.off()
+        self.led_up_left.off()
+        self.led_up_right.off()
+        self.led_middle.off()
+        self.led_down_left.off()
+        self.led_down_right.off()
+        self.led_down.off()
+
+
+    def dispDigit(self, digit):
+        self.reset()
+        self.dispMatrix(self.digits[digit])
+
+
+    def dispMatrix(self, matrix):
+        if matrix[0][0] == 1:
+            self.led_up.on()
+
+        if matrix[1][0] == 1:
+            self.led_up_left.on()
+
+        if matrix[1][1] == 1:
+            self.led_up_right.on()
+
+        if matrix[2][0] == 1:
+            self.led_middle.on()
+
+        if matrix[3][0] == 1:
+            self.led_down_left.on()
+
+        if matrix[3][1] == 1:
+            self.led_down_right.on()
+
+        if matrix[4][0] == 1:
+            self.led_down.on()
+
+        if matrix[5][0] == 1:
+            self.led_point.on()
+
+
+
+disp = Display(2,22,3,10,4,17,9,27)
+#disp.ledTest()
+
+n = 0
+while(True):
+    n = 0
+    while(n <= 9):
+        disp.dispDigit(n)
+        n += 1
+        sleep(1)
+
+
