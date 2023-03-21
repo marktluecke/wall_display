eb84ed3c790f39ce67eb02235d38dc7406bac400
marktluecke <henrijamespotter+gh@gmail.com>
2023-03-21 17:52:09 +0000

    html basic structure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from display import display as d
+from gpiozero import RGBLED
+
+led = RGBLED(red=26, green=19, blue=13)
 
 disp_unit_1 = d.DispUnit(2,22,3,10,4,17,9,27)
 disp_unit_2 = d.DispUnit(7,24,14,25,15,18,8,23)
@@ -18,7 +21,7 @@ def create():
 
     print(request.form['title'])
     disp.dispNumber(request.form['title'])
-    return(render_template('index.html', message=message))
+    return(redirect("/"))
 
 if __name__ == '__main__':
 	app.run(debug=True, host="0.0.0.0")
