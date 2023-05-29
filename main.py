66cdeec25b95caf5ea7bd8fec22b208f1a144d56
Henri <henri@waide.de>
2023-05-29 17:00:26 +0100

    first working web version

--- main.py
+++ main.py
@@ -11,18 +11,61 @@ disp = d.Display([disp_unit_1,disp_unit_2])
 
 app = Flask(__name__)
 
-@app.route("/")
+number = 0
+@app.route("/", methods=['GET', 'POST'])
 def index():
+    global number
     message = "Display your number"
-    return(render_template('index.html', message=message))
 
-@app.route("/", methods=('GET', 'POST'))
-def create():
-    message = "Test"
+    if request.method == 'POST':
+        if request.form.get('add_one') == '+1':
+            print("+1")
+            number += 1
+            pass
+        elif request.form.get('add_five') == '+5':
+            print('+5')
+            number += 5
+            pass
+        elif request.form.get('sub_one') == '-1':
+            print('-1')
+            number -= 1
+            pass
+        elif request.form.get('sub_five') == '-5':
+            print('-5')
+            number -= 5
+            pass
+        else:
+            pass
 
-    print(request.form['title'])
-    disp.dispNumber(request.form['title'])
-    return(redirect("/"))
+        num = request.form.get('count')
+        if num is not None:
+            number = int(num)
+        print(num)
+
+    elif request.method == 'GET':
+        return(render_template('index.html', message=message))
+
+    if number < 0:
+        number = 0
+    elif number > 99:
+        number = 99
+
+    disp.dispNumber(number)
+
+    return(render_template('index.html', message='done'))
+#@app.route("/", methods=('GET', 'POST'))
+#def create():
+#    message = "Test"
+
+#    if request.form['submit_button'] == '+1':
+#        print("+1")
+#        pass
+#    elif request.form['submit_button'] == '+5':
+#        pass
+
+#    print(request.form['title'])
+#    disp.dispNumber(request.form['title'])
+#    return(redirect("/"))
 
 if __name__ == '__main__':
 	app.run(debug=True, host="0.0.0.0")
